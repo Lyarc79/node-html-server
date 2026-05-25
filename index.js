6635f3d744ced6adce7274705bdf999f90729e8c
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-05-25 15:16:56 +0200

    Add all filepaths and helper function to avoid readFile repetition

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 import http from "node:http";
 import fs from "node:fs";
 
+const serverFile = (res, filePath, statusCode) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Internal Server Error");
+      return;
+    }
+    console.log(data);
+    res.writeHead(statusCode, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+};
+
 const server = http
   .createServer((req, res) => {
     if (req.url === "/") {
-      fs.readFile("./index.html", "utf8", (err, data) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log(data);
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
+      serverFile(res, "./index.html", 200);
+    } else if (req.url === "/about") {
+      serverFile(res, "./about.html", 200);
+    } else if (req.url === "/contact-me") {
+      serverFile(res, "./contact-me.html", 200);
+    } else {
+      serverFile(res, "./404.html", 404);
     }
   })
   .listen(8080, () => {
