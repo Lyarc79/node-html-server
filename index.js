95cc49647446185dd197bc50553cd1f8e4996b00
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-05-26 17:16:07 +0200

    Rewrite server using Express

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-import http from "node:http";
-import fs from "node:fs";
+import express from "express";
+import path from "path";
 
-const serverFile = (res, filePath, statusCode) => {
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Internal Server Error");
-      return;
-    }
-    console.log(data);
-    res.writeHead(statusCode, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-};
+const app = express();
 
-const server = http
-  .createServer((req, res) => {
-    if (req.url === "/") {
-      serverFile(res, "./index.html", 200);
-    } else if (req.url === "/about") {
-      serverFile(res, "./about.html", 200);
-    } else if (req.url === "/contact-me") {
-      serverFile(res, "./contact-me.html", 200);
-    } else {
-      serverFile(res, "./404.html", 404);
-    }
-  })
-  .listen(8080, () => {
-    console.log("Server connected");
-  });
+app.get("/", (req, res) =>
+  res.sendFile(path.join(import.meta.dirname, "index.html")),
+);
+app.get("/about", (req, res) =>
+  res.sendFile(path.join(import.meta.dirname, "about.html")),
+);
+app.get("/contact-me", (req, res) =>
+  res.sendFile(path.join(import.meta.dirname, "contact-me.html")),
+);
+app.use((req, res) =>
+  res.status(404).sendFile(path.join(import.meta.dirname, "404.html")),
+);
+
+const PORT = 8080;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Connected to port ${PORT}!`);
+});
